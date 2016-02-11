121f02722900def5ea5e14534083b3504ebc9d53
LendelTheGreat <langer.tomas@yahoo.com>
2016-02-11 21:01:41 +0000

    fix task init again

--- splitOrders.py
+++ splitOrders.py
@@ -21,7 +21,7 @@ def splitOrders(orders, max_payload, weights):
                     
                     o.items[i] = o.items[i] - max_items
             
-                newTask = Task(o.id, o.r, o.c, o.items[i], i)
+                newTask = Task(o.id, o.r, o.c, i, o.items[i])
                 tasks.append(newTask)
      
     return tasks
