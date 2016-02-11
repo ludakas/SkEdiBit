727c66c0fce851b127bfda7ab0c41d1cc70801fd
LendelTheGreat <langer.tomas@yahoo.com>
2016-02-11 19:24:59 +0000

    comment out tests

--- splitOrders.py
+++ splitOrders.py
@@ -41,23 +41,23 @@ def splitOrders(orders):
 class Object(object):
     pass
     
-dummy1 = Object()
-dummy1.r = 5
-dummy1.c = 5
-dummy1.noItems = 3
-dummy1.items = [0, 0, 1]
-dummyOrders = [dummy1]
+# dummy1 = Object()
+# dummy1.r = 5
+# dummy1.c = 5
+# dummy1.noItems = 3
+# dummy1.items = [0, 0, 1]
+# dummyOrders = [dummy1]
 
-dummy2 = Object()
-dummy2.r = 4
-dummy2.c = 4
-dummy2.noItems = 6
-dummy2.items = [0, 0, 1, 3, 3, 3]
-dummyOrders.append(dummy2)
+# dummy2 = Object()
+# dummy2.r = 4
+# dummy2.c = 4
+# dummy2.noItems = 6
+# dummy2.items = [0, 0, 1, 3, 3, 3]
+# dummyOrders.append(dummy2)
 
-tasks = splitOrders(dummyOrders)
+# tasks = splitOrders(dummyOrders)
 
-for t in tasks:
-    print str(t.r) + ' ' + str(t.c) + ' ' + str(t.type) + ' ' + str(t.count)
-    
-    
+# for t in tasks:
+   # print str(t.r) + ' ' + str(t.c) + ' ' + str(t.type) + ' ' + str(t.count)
+   
+   
