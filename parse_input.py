180bb9cd8b35fb3edadf00d873aed9ef399a3730
Jozef Mokry <jozef.mokry@gmail.com>
2016-02-11 19:40:51 +0000

    parseStuff defined

--- parse_input.py
+++ parse_input.py
@@ -10,6 +10,7 @@ class Warehouse(object):
         self.r = r
         self.c = c
         self.items = items
+        self.location = (r, c)
 
 class Order(object):
     """docstring for Order"""
@@ -19,54 +20,52 @@ class Order(object):
         self.c = c
         self.n_items = n_items
         self.items = items
-        
+        self.location = (r, c)
+
 
 def str2int(string_list):
     return map(lambda x: int(x), string_list)
 
+def parseStuff():
+  f = open('input/mother_of_all_warehouses.in', 'r')
+  n_rows, n_columns, n_drones, turns, max_payload = str2int(f.readline().split())
+  n_product_types = int(f.readline())
+  weights = f.readline().split()
+  weights = str2int(weights) # map(lambda x: int(x), weights)
+  n_warehouses = int(f.readline())
 
-f = open('input/mother_of_all_warehouses.in', 'r')
-
-
-
-
-n_rows, n_columns, n_drones, turns, max_payload = str2int(f.readline().split())
-n_product_types = int(f.readline())
-weights = f.readline().split()
-weights = str2int(weights) # map(lambda x: int(x), weights)
-n_warehouses = int(f.readline())
-
-warehouses = []
-for i in xrange(n_warehouses):
-    r, c = str2int( f.readline().split() )
-    items = str2int( f.readline().split() )
-    warehouses.append(Warehouse(r,c,items))
+  warehouses = []
+  for i in xrange(n_warehouses):
+      r, c = str2int( f.readline().split() )
+      items = str2int( f.readline().split() )
+      warehouses.append(Warehouse(r,c,items))
 
 
-n_orders = int(f.readline())
+  n_orders = int(f.readline())
 
-orders = []
-for i in range(n_orders):
-    r,c = str2int( f.readline().split() )
-    n_items = int( f.readline() )
-    items = np.zeros(n_product_types, dtype=int)
-    items_list = str2int( f.readline().split() )
-    for e in items_list:
-        items[e] += 1
+  orders = []
+  for i in range(n_orders):
+      r,c = str2int( f.readline().split() )
+      n_items = int( f.readline() )
+      items = np.zeros(n_product_types, dtype=int)
+      items_list = str2int( f.readline().split() )
+      for e in items_list:
+          items[e] += 1
 
-    orders.append(Order(r,c,n_items,items))
+      orders.append(Order(r,c,n_items,items))
 
+ #print 'n_rows',n_rows, 'n_columns',n_columns, 'n_drones',n_drones, 'turns',turns, 'max_payload',turns
+ #print 'n_product_types',n_product_types
 
+ #print 'n_warehouses',n_warehouses
+ #print 'warehouses[0].r',warehouses[0].r
 
-print 'n_rows',n_rows, 'n_columns',n_columns, 'n_drones',n_drones, 'turns',turns, 'max_payload',turns
-print 'n_product_types',n_product_types
+ #print 'n_orders',n_orders
+ #print 'orders[0].c',orders[0].c
+ #print 'orders[0].items[785]',orders[0].items[785]
 
-print 'n_warehouses',n_warehouses
-print 'warehouses[0].r',warehouses[0].r
+  f.close()
 
-print 'n_orders',n_orders
-print 'orders[0].c',orders[0].c 
-print 'orders[0].items[785]',orders[0].items[785]
+  return n_rows, n_columns, n_drones, turns, max_payload, n_product_types, weights, n_warehouses, warehouses, n_orders, orders
 
-f.close()
 
